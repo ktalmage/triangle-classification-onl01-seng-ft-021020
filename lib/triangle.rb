5cb47f34f0817bb1fcf226d83c3a03f32e76ae57
Kyle Talmage <ktalmage85@gmail.com>
2020-02-27 00:41:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,19 +10,15 @@ class Triangle
     
     def kind
       
+      
+    else
       if side_one == side_two && side_one == side_three
         return "equilateral".to_sym
       elsif side_two == side_three || side_one == side_three || side_one == side_two
         return "isosceles".to_sym
       elsif side_one != side_two && side_one != side_three
         return "scalene".to_sym
-        binding.pry
-      
         
-       begin 
-        raise Triangle::TriangleError
-      rescue Triangle::TriangleError => error
-      puts error.message
       
       end
     end
