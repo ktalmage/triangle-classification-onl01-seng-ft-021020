3d1d8ddadcc6bd653ebf115f0527e35ec2cd704f
Kyle Talmage <ktalmage85@gmail.com>
2020-02-27 00:30:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+    
+    def initialize(side_one,side_two,side_three)
+      @side_one = side_one
+      @side_two = side_two
+      @side_three = side_three
+    end
+    
+    def kind
+      total = side_one + side_two + side_three
+      if side_one == side_two && side_one == side_three
+        return "equilateral".to_sym
+      elsif side_two == side_three || side_one == side_three || side_one == side_two
+        return "isosceles".to_sym
+      elsif side_one != side_two && side_one != side_three
+        return "scalene".to_sym
+      elsif side_one == nil && side_two == nil && side_three == nil
+       begin 
+        raise TriangleError
+      rescue TriangleError => error
+      puts error.message
+      end
+    end
+  end
 end
+  
+
+ 
+     class TriangleError < StandardError
+      def method
+      "illegal"
+    end
+  end
+    
